07acf8ea4e968724ffff914d60ab94afa47101ab
Vladyslave <svlad2331@gmail.com>
2023-05-12 22:01:23 +0300

    Initial commit

---
+++ src/main/java/ua/project1/dao/BookDAO.java
@@ -0,0 +1,4 @@
+package ua.project1.dao;
+
+public class BookDAO {
+}
---
+++ src/main/java/ua/project1/model/People.java
@@ -0,0 +1,37 @@
+package ua.project1.model;
+
+public class People {
+    private int id;
+    private String name;
+    private String year;
+    public People(){}
+    public People(int id, String name, String year) {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
